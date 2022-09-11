73240eaa0d64cdc90481630c854260520c559c5e
wilkinsonmatt <wilkinsonmatt01@gmail.com>
2022-09-11 16:20:08 -0700

    Updated details to show all the details

--- tea-shop/src/components/TeaDetail.js
+++ tea-shop/src/components/TeaDetail.js
@@ -7,8 +7,10 @@ function TeaDetail(props){
   return (
     <React.Fragment>
       <h1>Tea Detail</h1>
-      <h3>{tea.origin} - {tea.name}</h3>
-      <p><em>{tea.price}</em></p>
+      <h4>Tea Name: {tea.name}</h4>
+      <h4>Tea Type: {tea.type}</h4>
+      <h4>Tea Origin: {tea.origin}</h4>
+      <h4>Price per ounce: {tea.price}</h4>
       <button onClick={()=> onClickingDelete(tea.id)}>Remove Tea</button>
       <hr/>
     </React.Fragment>
